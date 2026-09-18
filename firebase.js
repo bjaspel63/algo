fe66c06c91fcd02bfa61caf98187acf1f2661193
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2026-09-19 00:53:58 +0700

    Delete firebase.js

--- firebase.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getDatabase, ref, set, get, update, remove, onValue, runTransaction, push } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-import { firebaseConfig } from "./firebase-config.js";
-export const firebaseReady = !Object.values(firebaseConfig).some(v => String(v).includes("YOUR_"));
-export const db = firebaseReady ? getDatabase(initializeApp(firebaseConfig)) : null;
-export { ref, set, get, update, remove, onValue, runTransaction, push };
